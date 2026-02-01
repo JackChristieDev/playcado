c9d6776ad5b32a464407c42b58f9fb934f20ff3f
Jack Christie <jack.christie.dev@gmail.com>
2026-02-01 18:08:21 -0500

    Update playcado: 2026-02-01

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -73,9 +73,17 @@ android {
         }
         getByName("release") {
             // Logic for Play Store releases
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
             signingConfig = signingConfigs.getByName("release")
+            
+            ndk {
+                debugSymbolLevel = "full"
+            }
         }
     }
 }
